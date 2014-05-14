a50f0e00c6811f0ff0c4a2a92a277c13192f3ae5
Job van der Voort <jobvandervoort@gmail.com>
2014-05-14 23:43:25 +0200

    animations

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
+//= require jquery
 //= require_tree .
+
+
+$(document).ready(function() {
+
+  $('#logo').addClass('animated slideInLeft');
+
+  $('#lead-text').addClass('animated bounceInDown');
+
+  $('#list-form').addClass('animated fadeIn');
+
+});
